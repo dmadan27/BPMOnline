5f5592c948aeaf289829cf24bf3ef783772c536b
Ramadan Saputra <rarasta27@gmail.com>
2018-08-03 15:41:37 +0700

    module - get data schema
    => penambahan keterangan/komentar di setiap method

--- module/UsrGetDataSchema.js
+++ module/UsrGetDataSchema.js
@@ -1,15 +1,28 @@
 /**
  * Module untuk get data schema
+ * 
+ * Version: Alpha 0.1
+ * 
+ * How to Use:
+ * 1. Load Module
+ * 2. call method setColumn first, dan set column yg ingin di get datanya
+ * 3. call method getDataSchemaById
+ * 4. untuk get data dari hasil method, gunakan konsep callback js
+ * 
+ * Catatan:
+ * MASIH TAHAP DEVELOPMENT
+ * Komentari setiap console.log disaat module dipakai untuk Production
  */
 define("UsrGetDataSchemaModule", [], function() {
     return {
         /**
          * Property column
-         * sebagai penampung column untuk pencarian di schema
+         * sebagai penampung column apa saja untuk pencarian di schema
          */
         column: [],
         /**
-         * 
+         * Method init
+         * method test, hapus saat Production
          */
         init: function() {
             console.log("Execute Module UsrGetDataSchema");
@@ -54,13 +67,15 @@ define("UsrGetDataSchemaModule", [], function() {
             // get column yang sudah di set
             this.getColumn(schema);
             
-            // filter data dengan data yg ada di schema
+            // filter data yang dicari dengan data yg ada di schema
             var filters = Ext.create("Terrasoft.FilterGroup");
             filters.addItem(schema.createColumnFilterWithParameter(Terrasoft.ComparisonType.EQUAL, "Id", data));
             schema.filters = filters;
             schema.execute(function(resultExecute) {
+                // jika eksekusi sukses
                 if (resultExecute.success) {
                     if (resultExecute.collection.getCount() > 0) {
+                        // callback response
                         response({
                             status: true,
                             data: globalThis.setResponseExecute(resultExecute)
@@ -69,12 +84,14 @@ define("UsrGetDataSchemaModule", [], function() {
                         globalThis.column = [];
                     }
                     else {
+                        // callback response
                         response({status: false});
                         // reset column
                         globalThis.column = [];    
                     }
                 }
                 else {
+                    // callback response
                     response({status: false});
                     // reset column
                     globalThis.column = [];
@@ -84,15 +101,15 @@ define("UsrGetDataSchemaModule", [], function() {
         /**
          * Method untuk set data respone di result execute
          * @param resultExecute: var hasil execute di method getDataSchema
-         * @param column: property column
+         * return data berupa object yang sesuai dgn column yg sudah si set
          */
         setResponseExecute: function(resultExecute) {
             var result = {};
             this.column.forEach(function(item) {
-                // console.log(item);
+                // add property baru ke object result sesuai dgn item yg ada di column
                 Object.defineProperty(result, item, { value: resultExecute.collection.getByIndex(0).get(item) });
             });
-
+            
             console.log("Result method setResponseExecute: ");
             console.log(result);
             console.log("----------------------------------");
